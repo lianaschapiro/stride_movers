ffce9d4d27ab1495b24bc391e9835f8e88b61c8d
lianaschapiro <liana.schapiro@gmail.com>
2016-03-27 11:59:16 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+# gem 'mail'
+gem 'sendgrid-ruby'
+# gem 'sinatra-flash'
+# gem 'valid_email'
+
+# gem "activerecord"
+# gem "sinatra-activerecord"
+# gem "rake"
+# gem "sqlite3"
---
+++ main.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'sendgrid-ruby'
+# require 'sinatra/flash'
+# require 'valid_email'
+# require 'valid_email/validate_email'
+# set :port, 9494
+# enable :sessions
+
+get '/' do 
+  erb :home
+end
+
+post '/email' do 
+  # if ValidateEmail.valid?(params[:mail])
+    client = SendGrid::Client.new(api_key: ENV['SENDGRID_API_KEY'])
+    email = SendGrid::Mail.new do |m|
+      m.to      = 'liana.schapiro@gmail.com'
+      m.from    = params[:mail]
+      m.subject = 'Email Sent From Stride Movers'
+      m.text    = "Name: #{params[:name]}\r\n\r\nPhone: #{params[:number]}\r\n\r\nBody: #{params[:comment]}"
+    end
+  if client.send(email)
+    flash[:notice]="You've messaged me, I'll get back to you asap!"
+    redirect '/'
+  else
+    flash[:notice]="Please use a properly formatted email address!"
+    redirect '/#quote'
+  end
+end
+
+get '/resume' do 
+  erb :resume
+end
