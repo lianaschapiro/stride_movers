d9bc987bfd1c4c64cb6b32f72a97b71eab86c11f
lianaschapiro <liana.schapiro@gmail.com>
2016-03-27 16:50:23 -0400

    Updating contact form

--- main.rb
+++ main.rb
@@ -18,31 +18,31 @@ post '/email' do
       m.from    = params[:mail]
       m.subject = 'Email Sent From Stride Movers'
       m.html    = "<style='font-size:16px'>
-      				<b> Name: </b> #{params[:name]}<br>
-      				<b> Email: </b> #{params[:mail]}<br>
-      				<b> Phone: </b> #{params[:phone]}<br>
-      				<b> Secondary Phone: </b> #{params[:phone2]}<br>
-      				<b> How did you hear about us: </b> #{params[:refer]}<br>
-      				<b> Date 1: </b> #{params[:date1]}<br>
-      				<b> Date 2: </b> #{params[:date2]}<br>
-      				<b> Date 3: </b> #{params[:date3]}<br>
-      				<b> Date details: </b> #{params[:datedetails]}<br>
-      				<b> Starting address: </b> #{params[:starting]}<br>
-      				<b> Stairs @ start: </b> #{params[:stairs_start]}<br>
-      				<b> Details @ start: </b> #{params[:stairdetails_start]}<br>
-      				<b> COI needed? </b> #{params[:certificate]}<br>
-      				<b> Ending address: </b> #{params[:ending]}<br>
-      				<b> Stairs @ end: </b> #{params[:stairs_end]}<br>
-					<b> Details @ end: </b> #{params[:stairdetails_end]}<br>    				
-      				<b> Deadlines: </b> #{params[:deadlines]}<br>
-      				<b> Furniture list: </b> #{params[:furniture]}<br>
-      				<b> Valuables: </b> #{params[:valuable]}<br>
-      				<b> Assembly? </b> #{params[:assemble]}<br>
-      				<b> Small boxes: </b> #{params[:smallbox]}<br>
-      				<b> Medium boxes: </b> #{params[:mediumbox]}<br>
-      				<b> Large boxes: </b> #{params[:largebox]}<br>
-      				<b> Wardrobe boxes: </b> #{params[:wardrobebox]}<br>
-      				<b> Other items: </b> #{params[:other_items]}<br>
+      				<b> Name: </b> #{params[:name]}<br><br>
+      				<b> Email: </b> #{params[:mail]}<br><br>
+      				<b> Phone: </b> #{params[:phone]}<br><br>
+      				<b> Secondary Phone: </b> #{params[:phone2]}<br><br>
+      				<b> How did you hear about us: </b> #{params[:refer]}<br><br>
+      				<b> Date 1: </b> #{params[:date1]}<br><br>
+      				<b> Date 2: </b> #{params[:date2]}<br><br>
+      				<b> Date 3: </b> #{params[:date3]}<br><br>
+      				<b> Date details: </b> #{params[:datedetails]}<br><br>
+      				<b> Starting address: </b> #{params[:starting]}<br><br>
+      				<b> Stairs @ start: </b> #{params[:stairs_start]}<br><br>
+      				<b> Details @ start: </b> #{params[:stairdetails_start]}<br><br>
+      				<b> COI needed? </b> #{params[:certificate]}<br><br>
+      				<b> Ending address: </b> #{params[:ending]}<br><br>
+      				<b> Stairs @ end: </b> #{params[:stairs_end]}<br><br>
+					<b> Details @ end: </b> #{params[:stairdetails_end]}<br><br>  				
+      				<b> Deadlines: </b> #{params[:deadlines]}<br><br>
+      				<b> Furniture list: </b> #{params[:furniture]}<br><br>
+      				<b> Valuables: </b> #{params[:valuable]}<br><br>
+      				<b> Assembly? </b> #{params[:assemble]}<br><br>
+      				<b> Small boxes: </b> #{params[:smallbox]}<br><br>
+      				<b> Medium boxes: </b> #{params[:mediumbox]}<br><br>
+      				<b> Large boxes: </b> #{params[:largebox]}<br><br>
+      				<b> Wardrobe boxes: </b> #{params[:wardrobebox]}<br><br>
+      				<b> Other items: </b> #{params[:other_items]}<br><br>
       				<b> Other info: </b> #{params[:other]}
       				</style>"
     end
