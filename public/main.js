1130391a6c6e0dba2a351be43adeaf9d690b56eb
lianaschapiro <liana.schapiro@gmail.com>
2016-04-11 10:11:23 -0400

    fixing nav bar drop down options

--- public/main.js
+++ public/main.js
@@ -1,16 +1,21 @@
+// $(document).ready(function(){
 
+	// Open and close the general rates modal
+	function openRatesModal() {
+	  $('#rates_outer_modal').fadeIn(200);
+	}
+	function closeRatesModal() {
+	  $('#rates_outer_modal').fadeOut(200);
+	}
 
-function openRatesModal() {
-  $('#rates_outer_modal').fadeIn(200);
-}
-
-
-function closeRatesModal() {
-  $('#rates_outer_modal').fadeOut(200);
-}
-
-$(document).ready(function(){
+	// Dropdown menus on the nav bar
+	$(".dropdown").mouseenter(function(){
+		$(".dropdown-content").slideDown('slow');
+	})
+	$(".dropdown").mouseleave(function(){
+		$(".dropdown-content").slideUp('slow');
+	})
 	$(".dropdown-content").click(function(){
 	    $(".dropdown-content").slideUp('slow');
 	})
-});
+// });
