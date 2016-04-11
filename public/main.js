83d991c230071210fdba481091d3286be8106727
lianaschapiro <liana.schapiro@gmail.com>
2016-04-10 20:02:57 -0400

    fixed nav bar

--- public/main.js
+++ public/main.js
@@ -1,8 +1,16 @@
+
+
 function openRatesModal() {
   $('#rates_outer_modal').fadeIn(200);
-  }
+}
 
 
 function closeRatesModal() {
   $('#rates_outer_modal').fadeOut(200);
 }
+
+$(document).ready(function(){
+	$(".dropdown-content").click(function(){
+	    $(".dropdown-content").slideUp('slow');
+	})
+});
