e6a30b044816a2127ff3c79a5e7bc34b887cbb76
lianaschapiro <liana.schapiro@gmail.com>
2016-05-09 11:49:44 -0400

    Fixing slideshow

--- public/main.js
+++ public/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 
 	// Background slideshow on landing page
-	setInterval(function() {
-		var random_num = Math.floor((Math.random() * 6) + 1);
- 		$('#big_image_background').css({"background-image" : "url('images/couple" + random_num + ".jpg')"});
-	}, 4000);
+	// setInterval(function() {
+	// 	var random_num = Math.floor((Math.random() * 6) + 1);
+ // 		$('#big_image_background').css({"background-image" : "url('images/couple" + random_num + ".jpg')"});
+	// }, 4000);
+	
+	$("#big_image_background").backstretch([
+    	"images/couple1.jpg",
+    	"images/couple2.jpg",
+    	"images/couple3.jpg", 
+    	"images/couple4.jpg", 
+    	"images/couple5.jpg",
+    	"images/couple6.jpg"      
+  	], {duration: 3000, fade: 2000, centeredY: false});
+
 
 	// Open and close the general rates modal
 	function openRatesModal() {
